d504e02905971d8472290fca72e193d68539954c
rg_nitro <rishabhgarg.20000@gmail.com>
2021-08-20 21:54:39 +0530

    account choose with signup ui

--- app/src/main/java/com/example/classattendancemanagement/MainActivity.java
+++ app/src/main/java/com/example/classattendancemanagement/MainActivity.java
@@ -2,13 +2,39 @@ package com.example.classattendancemanagement;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+
+    ImageView civTeacher, civStudent;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        civTeacher = findViewById(R.id.civStudent);
+
+        civTeacher.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, SignupStudent.class);
+                MainActivity.this.startActivity(intent);
+
+            }
+        });
+
+        civStudent = findViewById(R.id.civStudent);
+
+        civStudent.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, SignupTeacher.class);
+                MainActivity.this.startActivity(intent);
+            }
+        });
     }
 }
